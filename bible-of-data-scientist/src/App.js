0e6f95ee7dee52b268db21580bdaf8f32c487abe
Laby Damaro Camara <ldamaro98@gmail.com>
2021-01-18 19:00:41 +0100

    afficher les cours

--- bible-of-data-scientist/src/App.js
+++ bible-of-data-scientist/src/App.js
@@ -1,19 +1,29 @@
+import React, { Component } from 'react';
 import './App.css';
 import Course  from './components/CourseComponent';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { COURS  } from './shared/course';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="secondary">
-          <div className="container">
-            <NavbarBrand href="/">Bible Of Data Scientist</NavbarBrand>
-          </div>
-      </Navbar>
-      <Course />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      courses: COURS
+    };
+  }
+  render(){
+    return (
+      <div>
+        <Navbar dark color="secondary">
+            <div className="container">
+              <NavbarBrand href="/">Bible Of Data Scientist</NavbarBrand>
+            </div>
+        </Navbar>
+        <Course courses={this.state.courses} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- bible-of-data-scientist/src/components/CourseComponent.js
+++ bible-of-data-scientist/src/components/CourseComponent.js
@@ -1,89 +1,91 @@
-import React, { Component } from 'react';
-import { Media } from 'reactstrap';
+import React, { Component, useState } from 'react';
+import { Card, CardBody, CardText, CardTitle, Collapse, Button } from 'reactstrap';
+
+
 
 class Course extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            courses: [
-                {
-                    "week": 1,
-                    "date": "9/30/2019",
-                    "title": "WQU DS Module Introductory Video",
-                    "link": "https://www.youtube.com/watch?v=QUQSY7pmuSQ&feature=youtu.be"
-                },
-                {
-                    "week": 1,
-                    "date": "10/01/2019",
-                    "title": "1.1.1 Basic Programming and Variables",
-                    "link": "https://www.youtube.com/watch?v=3phV29yvWO8&feature=youtu.be"
-                },
-                {
-                    "week": 1,
-                    "date": "10/02/2019",
-                    "title": "1.1.2 Functions",
-                    "link": "https://www.youtube.com/watch?v=SytwpgZ5k6U&feature=youtu.be"
-                },
-                {
-                    "week": 1,
-                    "date": "10/03/2019",
-                    "title": "1.1.3 Logic & Conditional Expressions",
-                    "link": "https://www.youtube.com/watch?v=CyjsNEFjLVk"
-                },
-                {
-                    "week": 1,
-                    "date": "10/04/2019",
-                    "title": "1.1.4 Iteration & Conclusion",
-                    "link": "https://www.youtube.com/watch?v=MIjqvWgfdq8"
-                },
-                {
-                    "week": 1,
-                    "date": "10/04/2019",
-                    "title": "1.2.1 Data Structures (list)",
-                    "link": "https://www.youtube.com/watch?v=GGhWL_r_CSE"
-                },
-                {
-                    "week": 1,
-                    "date": "10/04/2019",
-                    "title": "1.2.2 Data Structures (tuple)",
-                    "link": "https://www.youtube.com/watch?v=jDuGDuzGkyE"
-                },
-                {
-                    "week": 1,
-                    "date": "10/04/2019",
-                    "title": "1.2.3 Data Structures (set)",
-                    "link": "https://www.youtube.com/watch?v=b4odPCnUcwA"
-                }
-            ]
-        }
-    }
+ constructor(props) {
+     super(props);
+     this.state = {
+         isOpen: false,
+         selectedCourse: null
+     }
+ }
+setIsOpen() {
+    this.setState({isOpen: true});
+}
+toggle = () => this.setIsOpen(!this.state.isOpen);
 
-    render(){
-        const courses = this.state.courses.map((course, index)=>{
+onCourseSelect(course) {
+    this.setState({ selectedCourse: course});
+}
+
+renderCourse(course) {
+    if (course != null)
+        return(
+            <div className="col-12 col-md-8 mt-3">
+                    <Card>
+                    <iframe width="auto" height="450" frameborder="1" src={course.linkiframe} allowfullscreen="allowfullscreen"></iframe>
+                        <CardBody>
+                            <CardTitle>{course.title}</CardTitle>
+                        </CardBody>
+                    </Card>
+                </div>
+        );
+    else
+        return(
+            <div></div>
+        );
+}
+
+render() {
+        const courses = this.props.courses.map((course, index)=>{
             return (
-                <div key={index} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src="./images/datascience1.jpg"/>
-                        </Media>
-                        <Media body className="ml-5">
-                            <Media heading>{course.title}</Media>
-                            <p>Date: {course.date} <br/>Week: {course.week}</p>
-                        </Media>
-                    </Media>
+                <div key={index} className="col-12 col-md-8 mt-3">
+                    <Card>
+                    <iframe width="auto" height="380" frameborder="1" src={course.linkiframe} allowfullscreen="allowfullscreen"></iframe>
+                        <CardBody>
+                            <CardTitle>{course.title}</CardTitle>
+                        </CardBody>
+                    </Card>
                 </div>
             );
         });
+
+        const titles = this.props.courses.map((course, index)=>{
+            return (
+               <li key={index} className="col-12 col-md-12 list-group-item form-control" 
+               onClick={() => this.onCourseSelect(course)}>
+                   {course.title}
+                </li>
+            );
+        });
         return (
-            <div className="container">
-                <Media list>
-                    {courses}
-                </Media>
+            <div className="container-fluid">
+                <div className="row mx-auto">
+
+
+                  <div className="col-12 col-md-4">
+                    <Button color="primary" className="form-control mt-3" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Module I</Button>
+                        <Collapse isOpen={this.state.isOpen}>
+                            
+                            <ul className="list-group">
+                                {titles}
+                            </ul>
+                          
+                        </Collapse>
+                    </div>
+
+
+                    {this.renderCourse(this.state.selectedCourse)}
+                       
+                </div>
+                
             </div>
         );
+    
     }
-
 }
 
 export default Course;
